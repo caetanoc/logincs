1c88c792bdff60e4165ee23d6c37710a8f7385d1
CLAUDIOMIRO SILVA <claudiomiro.silva2@etec.sp.gov.br>
2021-10-21 21:43:35 -0300

    incluir form creditos.

---
+++ LoginDB/Apresentacao/FormCredito.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginDB.Apresentacao
+{
+    public partial class FormCredito : Form
+    {
+        public FormCredito()
+        {
+            InitializeComponent();
+        }
+    }
+}
